4e4dc5da14d541ae577f092aad39a3529a82f95a
kumarUtsav-01 <kumarutsav171@gmail.com>
2022-10-01 15:31:42 +0530

    Master: Application set up complete. Installed and configured webpack and other dependencies to spin up a baic application which will be used a starting point in the project.

---
+++ src/index.js
@@ -0,0 +1,6 @@
+/* Testing the js file is loaded and applied in the bundle output */
+import "./index.css";
+
+const container = document.getElementById("root");
+
+container.innerText = "Application is running !!";
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  devServer: {
+    static: "./dist",
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [new HTMLWebpackPlugin({ template: "./src/index.html" })],
+};
