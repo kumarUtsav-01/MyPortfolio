f4f35d50c723ff4409146ae91cccf3fe448c4118
kumarUtsav-01 <kumarutsav171@gmail.com>
2022-10-22 07:06:23 +0530

    Modified the loader component to be responsively put at the center of document.

--- src/loader/loader.js
+++ src/loader/loader.js
@@ -4,12 +4,5 @@ export function loader() {
   const loader = document.createElement("div");
   loader.setAttribute("id", "loader");
 
-  // center the loader to the window
-  const top = document.documentElement.clientHeight / 2 - 60;
-  const left = document.documentElement.clientWidth / 2 - 60;
-
-  loader.style.top = top + "px";
-  loader.style.left = left + "px";
-
   return loader;
 }
